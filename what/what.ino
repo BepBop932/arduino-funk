5276d14dd27a05f55571f36da42a6ba32eab442e
bepbop932 <josee7040@gmail.com>
2023-10-07 20:03:05 -0400

    i swear im learning more but i feel stupider

--- what/what.ino
+++ what/what.ino
@@ -1,20 +1,22 @@
 #include <Servo.h>
+#include <LiquidCrystal.h>
 
-Servo x;
-
-int pp = 5;
-int pv;
-int npv;
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+String input;
 
 void setup() {
-    x.attach(2);
+    lcd.begin(16,2);
+    lcd.print("Hello, World!");
     Serial.begin(9600);
 }
 
 void loop() {
-    pv = analogRead(pp);
-    npv = map(pv, 0, 1023, 0, 180);
-    x.write(npv);
-//    delay(15);
-    Serial.println(npv);
+    if(Serial.available()){
+        Serial.print("type thing:");
+        input = Serial.read();
+    }
+    lcd.clear();
+    lcd.write(input);
+    delay(1000);
 }
